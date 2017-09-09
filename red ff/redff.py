2d331fcd4e6d8b2a3ab7d90ec79f9edc88b8f72c
AtalGuzman <adolfo.guzman@usach.cl>
2017-09-09 16:56:57 -0300

    Primera lectura de archivo

---
+++ red ff/redff.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas
+pacient01H = pandas.read_csv("Data\Hipercapnia\HC090161.csv", delim_whitespace  = True)
+
+#pacient02H = np.genfromtext('\Data\Hipercapnia\HC091161.PAR', delimiter = " ")
+#pacient03H = np.genfromtext('\Data\Hipercapnia\HC092161.PAR', delimiter = " ")
+#pacient04H = np.genfromtext('\Data\Hipercapnia\HC093161.PAR', delimiter = " ")
+#pacient05H = np.genfromtext('\Data\Hipercapnia\HC094161.PAR', delimiter = " ")
+#pacient06H = np.genfromtext('\Data\Hipercapnia\HC095161.PAR', delimiter = " ")
+#pacient07H = np.genfromtext('\Data\Hipercapnia\HC096161.PAR', delimiter = " ")
+#pacient08H = np.genfromtext('\Data\Hipercapnia\HC097161.PAR', delimiter = " ")
+#pacient09H = np.genfromtext('\Data\Hipercapnia\HC098161.PAR', delimiter = " ")
+#pacient10H = np.genfromtext('\Data\Hipercapnia\HC099161.PAR', delimiter = " ")
+
+print(pacient01H)
+#Leer los datos
